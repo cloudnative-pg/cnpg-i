70ed37004b6672a5fd35ef69ea482681a6d5dd5d
Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>
2024-01-16 17:26:23 +0000

    feat: operator reconciler specification
    Signed-off-by: Leonardo Cecchi <leonardo.cecchi@enterprisedb.com>

--- scripts/build.sh
+++ scripts/build.sh
@@ -5,10 +5,7 @@ cd "$(dirname "$0")/.." || exit
 # Recompile protobuf specification
 protoc --go_out=. --go_opt=module=github.com/cloudnative-pg/cnpg-i \
     --go-grpc_out=. --go-grpc_opt=module=github.com/cloudnative-pg/cnpg-i \
-    proto/wal.proto
-protoc --go_out=. --go_opt=module=github.com/cloudnative-pg/cnpg-i \
-    --go-grpc_out=. --go-grpc_opt=module=github.com/cloudnative-pg/cnpg-i \
-    proto/identity.proto
+    proto/*.proto
 
 # Compile client
 go build `go list ./... | grep -v 'vendor'`
